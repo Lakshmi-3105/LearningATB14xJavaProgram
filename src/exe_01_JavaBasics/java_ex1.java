3caa432ad5841d55c239bb93602f77652d83d87f
Lakshmi-3105 <jayagarlapati2002@gmail.com>
2025-09-19 14:35:17 +0530

    java_exampel1

---
+++ src/exe_01_JavaBasics/java_ex1.java
@@ -0,0 +1,14 @@
+package exe_01_JavaBasics;
+
+public class java_ex1 {
+    public static void main(String[] args) {
+        byte b =11;
+        short s= 24;
+        int i=2;
+        char c='A';
+        long l= 23456l;
+        float f=23.4f;
+
+
+    }
+}
